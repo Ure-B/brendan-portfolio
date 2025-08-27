beeb958e355a8ec41c5fcfa38ccdb0f60487dab6
Ure-B <brendandavid1@gmail.com>
2025-08-26 18:19:20 -0600

    .

--- portfolio-website/src/components/Intro.jsx
+++ portfolio-website/src/components/Intro.jsx
@@ -4,7 +4,7 @@ function Intro() {
 
     return (
         <div class="">
-            <div class="w-screen h-screen relative z-0 bg-cover bg-no-repeat bg-center bg-[url('brendan-portfolio/background1.png')]">
+            <div class="w-screen h-screen relative z-0 bg-cover bg-no-repeat bg-center bg-[url('/background1.png')]">
                 <div class=""></div>
             </div>
         </div>
